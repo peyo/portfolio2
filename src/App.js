beab7072fb909648e369e46a04a6eef38ba084a1
peyo <peteryyoon@gmail.com>
2020-09-28 02:00:02 -0700

    animated cursor

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import CellierPage from "./views/ProjectPage/CellierPage";
 import WordPage from "./views/ProjectPage/WordPage";
 import ResumePage from "./views/ResumePage/ResumePage";
 
+// route transition
+import { spring, AnimatedSwitch } from "react-router-transition";
+
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -16,13 +19,54 @@ export default class App extends Component {
     return (
       <div className="app">
         <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/project-page/mech" component={MechPage} />
-          <Route path="/project-page/cellier" component={CellierPage} />
-          <Route path="/project-page/word" component={WordPage} />
-          <Route path="/resume-page/resume" component={ResumePage} />
+          <AnimatedSwitch
+            atEnter={bounceTransition.atEnter}
+            atActive={bounceTransition.atActive}
+            mapStyles={mapStyles}
+            className="route-wrapper"
+          >
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/project-page/mech" component={MechPage} />
+            <Route path="/project-page/cellier" component={CellierPage} />
+            <Route path="/project-page/word" component={WordPage} />
+            <Route path="/resume-page/resume" component={ResumePage} />
+          </AnimatedSwitch>
         </Switch>
       </div>
     );
   }
 }
+
+function mapStyles(styles) {
+  return {
+    opacity: styles.opacity,
+    transform: `scale(${styles.scale})`,
+  };
+}
+
+// wrap the `spring` helper to use a bouncy config
+function bounce(val) {
+  return spring(val, {
+    stiffness: 330,
+    damping: 22,
+  });
+}
+
+// child matches will...
+const bounceTransition = {
+  // start in a transparent, upscaled state
+  atEnter: {
+    opacity: 0,
+    scale: 1.2,
+  },
+  // leave in a transparent, downscaled state
+  atLeave: {
+    opacity: bounce(0),
+    scale: bounce(0.8),
+  },
+  // and rest at an opaque, normally-scaled state
+  atActive: {
+    opacity: bounce(1),
+    scale: bounce(1),
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import ScrollToTop from "./misc/ScrollToTop"
 import App from "./App";
+
+// scroll to top
+import ScrollToTop from "./misc/ScrollToTop"
+
+// animated cursor
+import AnimatedCursor from "react-animated-cursor"
 
 ReactDOM.render(
   <BrowserRouter>
     <ScrollToTop />
+    <AnimatedCursor
+      innerSize={9}
+      outerSize={9}
+      color='193, 11, 111'
+      outerAlpha={0.3}
+      innerScale={0.6}
+      outerScale={5}
+    />
     <App />
   </BrowserRouter>,
   document.getElementById("root")
