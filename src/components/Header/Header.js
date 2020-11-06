3e8b44c42a1ba4f23a7ef9bb8626c58bebd035bc
peyo <peteryyoon@gmail.com>
2020-11-05 21:53:20 -0800

    hamburger overlay update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,57 +18,53 @@ export default class Header extends Component {
     return (
       <section className="header__header">
         <div className="header__navbar-container">
-        <Navbar className="header__navbar">
-          <Navbar.Brand>
-            <img alt="peyo's logo" src={Logo} className="header__logo" />{" "}
-          </Navbar.Brand>
-          <Menu right>
-            <Nav className="mr-sm-2">
-              <Nav.Link
-                href="https://www.linkedin.com/in/peteryyoon/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div className="header__navLink">
-                  linkedin
-                </div>
-              </Nav.Link>
-              <Nav.Link
-                href="https://github.com/peyo"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div className="header__navLink">
-                  github
-                </div>
-              </Nav.Link>
-              <Nav.Link
-                href="https://leetcode.com/problems/maximal-network-rank/discuss/903980/(Javascript)-4-steps-including-base-case.-(96-ms-faster-than-98.79-43.5-MB-less-than-5.45)"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div className="header__navLink">
-                  leetcode
-                </div>
-              </Nav.Link>
-              <Nav.Link
-                href="/resume-page/resume"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div className="header__navLink">
-                  resume
-                </div>
-              </Nav.Link>
-            </Nav>
-          </Menu>
+          <Navbar className="header__navbar">
+            <Navbar.Brand>
+              <img alt="peyo's logo" src={Logo} className="header__logo" />{" "}
+            </Navbar.Brand>
+            <Menu right width={"210px"} noOverlay>
+              <Nav className="mr-sm-2">
+                <Nav.Link
+                  href="https://www.linkedin.com/in/peteryyoon/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="header__navLink">linkedin</div>
+                </Nav.Link>
+                <Nav.Link
+                  href="https://github.com/peyo"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="header__navLink">github</div>
+                </Nav.Link>
+                <Nav.Link
+                  href="https://leetcode.com/problems/maximal-network-rank/discuss/903980/(Javascript)-4-steps-including-base-case.-(96-ms-faster-than-98.79-43.5-MB-less-than-5.45)"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="header__navLink">leetcode</div>
+                </Nav.Link>
+                <Nav.Link
+                  href="/resume-page/resume"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="header__navLink">resume</div>
+                </Nav.Link>
+              </Nav>
+            </Menu>
           </Navbar>
         </div>
         <div className="header__container">
           <div className="header__helloWorld">Hello World. I'm Peter.</div>
           <div className="header__spacing1"></div>
           <div className="header__shortBio">
-            I believe curiosity and openness, paired with self-discipline and diligence, lead to great outcomes. These traits afford us the ability to tackle new challenges. Check out my explorations, applying curiosity and self-discipline to create simple to complex solutions.
+            I believe curiosity and openness, paired with self-discipline and
+            diligence, lead to great outcomes. These traits afford us the
+            ability to tackle new challenges. Check out my explorations,
+            applying curiosity and self-discipline to create simple to complex
+            solutions.
           </div>
           <div className="header__spacing2"></div>
         </div>
