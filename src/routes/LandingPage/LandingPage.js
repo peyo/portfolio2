bd38317c2660ad5e8f4731144403c1245bc0a360
peyo <peteryyoon@gmail.com>
2020-11-06 19:03:09 -0800

    google analytics update

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -6,11 +6,6 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import ProjectSection from "../../components/ProjectSections/ProjectSection";
 
-// google analytics
-import ReactGA from "react-ga";
-ReactGA.initialize("UA-179253130-1");
-ReactGA.pageview(window.location.pathname + window.location.search);
-
 export default class LandingPage extends Component {
   render() {
     return (
