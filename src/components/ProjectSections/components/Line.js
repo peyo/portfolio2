63ad4f585b2b98cf5996bc1a76a360480bc8068e
peyo <peteryyoon@gmail.com>
2020-11-20 13:31:17 -0800

    lazy load

--- src/components/ProjectSections/components/Line.js
+++ src/components/ProjectSections/components/Line.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
 import "../../../assets/css/everything.css";
 
+// animations and radium
+import { fadeInRight } from "react-animations";
+import Radium, { StyleRoot } from "radium";
+
+// lazyload
+import LazyLoad from "react-lazyload";
+
+const styles = {
+  fadeInRight: {
+    animation: "x 1s",
+    animationName: Radium.keyframes(fadeInRight, "fadeInRight")
+  }
+}
+
 export default class Line extends Component {
   render() {
     return (
-      <section className="line">
-        <div className="line__line">
-        </div>
-      </section>
+      <LazyLoad once offset={100}>
+        <StyleRoot>
+          <section className="line">
+            <div className="line__line" style={styles.fadeInRight}>
+            </div>
+          </section>
+        </StyleRoot>
+      </LazyLoad>
     );
   }
 }
