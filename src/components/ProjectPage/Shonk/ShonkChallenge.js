c4aa15e3b5f7c9255aef906042eaee19991e1b5b
peyo <peteryyoon@gmail.com>
2020-12-19 11:08:11 -0800

    Edited copy on Shonk project page.

--- src/components/ProjectPage/Shonk/ShonkChallenge.js
+++ src/components/ProjectPage/Shonk/ShonkChallenge.js
@@ -11,9 +11,9 @@ export default class ShonkChallenge extends Component {
           </div>
           <div className="shonkChallenge__spacing1"></div>
           <div className="shonkChallenge__description">
-            Create a link shortener application. The goal was to build a full-stack React web app to write data through a GraphQL back-end. Once a user created data through the form, the inputted information would appear in a list below the form. I also limited myself to mobile-first responsive layout and styling using Material UI.
+            Create a link shortener application. The goal was to build a full-stack React web app to write data through a GraphQL back-end. Once a user created data through a form, the inputted information would appear in a list below the form. I also applied a mobile-first responsive layout and styling using Material UI.
 
-            Non-requirements: The challenge did not expect a fully working URL shortener. For example, it is not required for a working redirect when clicking on a shortened URL.
+            Non-requirements: I did not expect a fully working URL shortener. For example, it was not required for a working redirect when clicking on a shortened URL.
           </div>
         </div>
       </section>
